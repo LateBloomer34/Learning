adbf7fe7a533da22f3e5e835eda149b7931d0e9e
LateBloomer34 <himnariyal30@gmail.com>
2025-08-26 11:14:17 +0530

    01_Count-digit-in-a-number.cpp

--- DSA/01_Aug26/Basic_Maths/01_Count-digit-in-a-number.cpp
+++ DSA/01_Aug26/Basic_Maths/01_Count-digit-in-a-number.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
+int countNum(int num){
+    int count = 0 ;
+    while (num > 0 || num<0){ // condition for bot -ve and +ve numbers
+        count++;
+        num  = num/10;
+    }
+    return count;
+}
 
 int main (){
-    cout<<"himanshu";
+    int num ; 
+    cout<<"enter the number ";
+    cin>>num;
+   cout<< countNum(num);
 }
