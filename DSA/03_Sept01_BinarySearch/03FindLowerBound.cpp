0779ab086b0c917f705cd0d59f3e9c6368046133
LateBloomer34 <himnariyal30@gmail.com>
2025-09-01 16:23:24 +0530

    find lower bound in a sorted array

--- DSA/03_Sept01_BinarySearch/03FindLowerBound.cpp
+++ DSA/03_Sept01_BinarySearch/03FindLowerBound.cpp
@@ -7,17 +7,19 @@ int lb (vector<int>arr , int target){
     int n = arr.size();
     int start = 0 ; 
     int end = n-1;
+    int result = -1;
 
     while(start<=end){
         int mid = start+(end-start)/2;
-        if (target>arr[mid]){
-            start = mid+1;
-        }
-        else{
+        if (target<arr[mid]){
+            result = mid;
             end = mid-1;
         }
+        else{
+            start = mid+1;
+        }
     }
-    return start;
+    return result;
 }
 
 int main (){
