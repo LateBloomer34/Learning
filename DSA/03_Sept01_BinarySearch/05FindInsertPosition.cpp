874c06c834838d97347db9c88608ac54648450d6
LateBloomer34 <himnariyal30@gmail.com>
2025-09-02 10:42:11 +0530

    find insert position

--- DSA/03_Sept01_BinarySearch/05FindInsertPosition.cpp
+++ DSA/03_Sept01_BinarySearch/05FindInsertPosition.cpp
@@ -8,23 +8,21 @@ int insertEle(vector<int>arr , int target){
     int start = 0 ; 
     int end = n-1;
     int ans = n; // default: insert at end
-
     while(start<=end){
         int mid = start+(end-start)/2;
-        int res = guess(mid);
-        if (res==0){
+        if (target<=arr[mid]){
             ans = mid;
-            end = mid-1;
+            end= mid-1;
         }
         else{
             start = mid+1;
         }
     }
-    return ans;
+return ans;
 }
 
 int main (){
     vector<int>arr = {1,2,4,7};
-    int target = 6;
+    int target = 4;
     cout<<insertEle(arr , target);
 }
