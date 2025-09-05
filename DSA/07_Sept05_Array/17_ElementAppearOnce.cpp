0814a5122e49c67d3f9d2969bc7eab6acebd1200
LateBloomer34 <himnariyal30@gmail.com>
2025-09-05 14:27:22 +0530

    element appear once other twice

---
+++ DSA/07_Sept05_Array/17_ElementAppearOnce.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int appearOnce(vector<int>arr ){
+    int n = arr.size();
+    int ele = 0;
+    for (int i = 0 ; i < n ; i++){
+        ele = ele^arr[i];
+    }
+    return ele;
+}
+
+int main(){
+    vector<int>arr = {2,1,2};
+    cout<<appearOnce(arr);
+}
