f5f1ca08bad6080afefe37002873b23fc57e7be2
LateBloomer34 <himnariyal30@gmail.com>
2025-09-06 11:44:48 +0530

    union of two sorted array

---
+++ DSA/08_Sept06_Array/01_unionOfTwoArray.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int unionArr(vector<int>arr1 , vector<int>arr2){
+    int n = arr1.size();
+    int m = arr2.size();
+    vector<int>uni = {};
+    int i = 0 ;
+    int j = 0;
+
+    while(i<n && j<m){
+        if (arr1[i]<=arr2[j]){
+        if (uni.empty() || uni.back()!= arr1[i]){
+            uni.push_back(arr1[i]);
+        }
+        i++;
+        }
+        else{
+            if(uni.empty() || uni.back()!=arr2[j]){
+                uni.push_back(arr2[j]);
+            }
+            j++;
+        }
+    }
+    // if no of element in arr1 is more than arr2
+    while(i<n){
+        if (uni.empty() || uni.back()!=arr1[i]){
+            uni.push_back(arr1[i]);
+        }
+        i++;
+    }
+    // if arr2 element is more than arr2
+    while(j<m){
+        if (uni.empty() || uni.back()!=arr2[j]){
+            uni.push_back(arr2[j]);
+        }
+        j++;
+
+    }
+    for(int i = 0 ; i<uni.size() ; i++){
+        cout<<uni[i]<<" ";
+    }
+return 0;
+}
+
+int main (){
+    vector<int>arr1 = {1};
+    vector<int>arr2 = {2};
+    unionArr(arr1 , arr2);
+}
