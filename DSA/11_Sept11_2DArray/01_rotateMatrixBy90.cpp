6fba5c9db1820324eb7597c50222647526edaf86
LateBloomer34 <himnariyal30@gmail.com>
2025-09-11 10:50:09 +0530

    rotate matrix by 90

---
+++ DSA/11_Sept11_2DArray/01_rotateMatrixBy90.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void rotate(vector<vector<int>>arr){
+    int n = arr.size();
+    int m = arr[0].size();
+
+    // transpose of matrix;
+    for (int i = 0 ; i < n ; i++){
+        for (int j = i+1 ; j<m ;j++ ){
+            int temp  =arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i]= temp;
+        }
+    }
+
+    // reverse rows
+
+    for (int i = 0 ;i < n ; i++){
+        int start = 0 ;
+        int end = n-1;
+        while(start<end){
+            int temp = arr[i][start];
+            arr[i][start]  = arr[i][end];
+            arr[i][end] =temp;
+            start++;
+            end--;
+        }
+    }
+
+    for (int i = 0 ; i< n ; i++){
+        for (int j = 0 ; j < m; j++){
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+
+}
+
+int main (){
+    vector<vector<int>>arr = {{1,2,3},{4,5,6},{7,8,9}};
+    rotate(arr);
+}
