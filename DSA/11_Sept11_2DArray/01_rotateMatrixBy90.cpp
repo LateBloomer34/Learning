f55344c452a25191b51df4fadcfad8df51a7fed6
LateBloomer34 <himnariyal30@gmail.com>
2025-09-12 11:14:12 +0530

    majority element by n/3

--- DSA/11_Sept11_2DArray/01_rotateMatrixBy90.cpp
+++ DSA/11_Sept11_2DArray/01_rotateMatrixBy90.cpp
@@ -20,7 +20,7 @@ void rotate(vector<vector<int>>arr){
 
     for (int i = 0 ;i < n ; i++){
         int start = 0 ;
-        int end = n-1;
+        int end = m-1;
         while(start<end){
             int temp = arr[i][start];
             arr[i][start]  = arr[i][end];
