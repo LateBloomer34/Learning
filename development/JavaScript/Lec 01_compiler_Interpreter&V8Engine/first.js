ae84900b2084022def2b2e107e854c87aac90ff9
LateBloomer34 <himnariyal30@gmail.com>
2025-11-02 14:22:39 +0530

    what is compiler interpreter & V8 engine

---
+++ development/JavaScript/Lec 01_compiler_Interpreter&V8Engine/first.js
@@ -0,0 +1,16 @@
+// diff between inerpreter and compiler 
+
+/*
+Compiler - convert source code into machine code at once, compiler identify all errors after all initial translation.
+
+Interprator- convert source code into machine code line by line , it stops at first error they find which is easier for debug.
+
+
+V8 Engine- available in chrome browser which is use for compline and execute JS code  , which is help us to run source code inside the browser.
+
+if we need to run JS code outside the browser we need to insall a compiler for it, so need to be install node seperately in the machine. 
+
+
+/// to check is node install in our machine use the command node --v  in terminal 
+
+*/
