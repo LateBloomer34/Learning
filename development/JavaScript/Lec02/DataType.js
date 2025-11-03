810aa4bbfa4594767b36274f4f55acf60e86d469
LateBloomer34 <himnariyal30@gmail.com>
2025-11-03 09:10:54 +0530

    JS Datatype

---
+++ development/JavaScript/Lec02/DataType.js
@@ -0,0 +1,16 @@
+/* 
+number
+string
+boolean
+undefined
+null
+symbol
+BigInt
+
+
+we can store max 64 bit number  inside the number datatype
+
+*/
+
+console.log(typeof BigInt(4654));
+console.log(typeof 4565n);
