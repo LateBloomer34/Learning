e3b2096359deacdfb454f936de4bba490e65cca8
LateBloomer34 <himnariyal30@gmail.com>
2025-11-04 12:36:47 +0530

    creation of class

---
+++ Problem_Solving_CPP/Learning/Day01_OOPS/creationOfClass01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class student {
+    public:
+    string name = "himanshu";
+    int age= 36;
+    string branch = "cse";
+};
+
+int main (){
+    student details;
+cout<<details.name<<endl; 
+cout<<details.age<<endl;
+details.name = "raju";
+cout<<details.name<<endl;
+}
