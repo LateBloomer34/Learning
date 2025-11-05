f7a1f5d15d46802406c88d4850c62b83322671e4
LateBloomer34 <himnariyal30@gmail.com>
2025-11-05 11:32:23 +0530

    constructor

---
+++ Problem_Solving_CPP/Learning/Day01_OOPS/Constructor06.cpp
@@ -0,0 +1,46 @@
+// constructor is a special function/method in a class which called automatically when onject is created;
+
+
+// keypoint of constructor
+// constructor name is always same as class name 
+// it is a function but it doest not have any return type
+// it run automatically when created;
+
+
+
+// types of constructor
+// default
+// parameterize
+// copy constructor
+
+
+
+// creation of constructor
+
+
+
+#include<iostream>
+using namespace std;
+
+class car{
+    string brand;;
+    string color;
+
+public:
+// constructor -  is a function same name as class but dont have any return type
+    car(){
+        brand = "tesla";
+        color = "red";
+        cout<<"constructor created";
+    }
+
+};
+
+
+
+int main (){
+     car first;
+     
+
+    return 0;
+}
---
+++ Problem_Solving_CPP/Learning/Day01_OOPS/constructorType01.cpp
@@ -0,0 +1,21 @@
+// default constructor
+
+#include<iostream>
+using namespace std;
+
+class car{
+    public:
+    string brand;
+    string color;
+
+    car(){
+       // here car is a constructor which is a function doent have any return type and it eqecute automatically
+        cout<<"construcor created ";
+    }
+
+};
+
+int main (){
+    car first;
+    
+}
