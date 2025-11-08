971ebe3a6fd713e24f8266ec096a8581a38cbc98
LateBloomer34 <himnariyal30@gmail.com>
2025-11-08 10:04:52 +0530

    memory managemnet in object

---
+++ development/JavaScript/Lect06_Memory_Management/memoryManagementInObj.js
@@ -0,0 +1,20 @@
+const obj1 = {
+    name:"himanshu",
+    class:"first"
+}
+console.log(obj1);
+obj1.name = "raju"; // coz stack store only address of object, here we need to change the value not address,
+console.log(obj1);
+
+let obj = {
+    name :"vipin",
+    class:"two",
+}
+console.log(obj);
+// obj1 = obj
+
+// console.log(obj);
+// console.log(obj1);
+
+
+
