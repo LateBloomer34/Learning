f4ce3158db535948f6e30ae1618e4f07efa21c1d
LateBloomer34 <himnariyal30@gmail.com>
2025-11-08 09:49:01 +0530

    memory management

---
+++ development/JavaScript/Lect06_Memory_Management/first.js
@@ -0,0 +1,23 @@
+/*
+    => Memory Management
+    
+stack and heap
+
+all variable, function paraeter , return value,  adress of non primitive store in stack, 
+stack work on the principle of LIFO.
+automatic allocation / deallocation.
+faster speed , lifitime limited .
+the memeory size of stack is very low 
+
+
+
+heap - all non primitive (object function, array) store in heap memory.
+size of heap is much larger than stack ,
+no fifo lifo use here.
+
+
+if we want to updat ethe value of any vairable , that varibel is not update it will store in new memory location and left the old location.
+
+
+
+*/
