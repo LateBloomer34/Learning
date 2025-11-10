44a8a3b79f29f7d4818a847e86d220866a9ee55d
LateBloomer34 <himnariyal30@gmail.com>
2025-11-10 11:53:30 +0530

    math in JS

---
+++ development/JavaScript/lect09_Array/Rev_Maths.js
@@ -0,0 +1,15 @@
+// generate random number between 50 to 60;
+
+
+let min = 50 ;
+let max = 60;
+
+//using math.floor()
+console.log(Math.floor((Math.random()*(max-min+1))+min));
+
+//using math.ceil();
+
+
+
+console.log(Math.ceil(Math.random()*(max-min+1)+(min-1)));
+
