277e38eea0cc1deb710e38b238d6bc395a0d9a12
LateBloomer34 <himnariyal30@gmail.com>
2025-11-18 15:53:44 +0530

    Hoisting in JS

---
+++ development/JavaScript/lect16Hoisting/01hoisting.js
@@ -0,0 +1,13 @@
+// Hoisting is a JavaScript behavior where variable and function declarations are moved (“hoisted”) to the top of their scope during the compilation phase.
+
+// JS is a synchronous single threades language
+
+// console.log(a);
+
+//  a = 5;
+
+
+// meet();
+// var meet  = function (){
+//     console.log("hello meet using let");
+//  }
