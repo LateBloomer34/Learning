4b5acadc83f86e7f91c585810ef75b5ae16f9402
LateBloomer34 <himnariyal30@gmail.com>
2025-11-19 21:53:59 +0530

    DOM

---
+++ development/JavaScript/Lect18_DOM/first.js
@@ -0,0 +1,5 @@
+let ele = document.getElementById("heading2");
+ele.innerHTML="topic covered so far";
+ele.style.backgroundColor = "red";
+ele.style.fontSize = "50px";
+ele.innerText = "m sikh ke rahunga"
