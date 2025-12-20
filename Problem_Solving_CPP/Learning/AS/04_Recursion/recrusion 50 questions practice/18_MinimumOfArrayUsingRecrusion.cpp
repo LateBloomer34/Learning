96884b5fc4e0f88ced891e38e4d7ad0534cc6fb4
LateBloomer34 <himnariyal30@gmail.com>
2025-12-20 20:38:58 +0530

    minimum of array using recrusion

--- Problem_Solving_CPP/Learning/AS/04_Recursion/recrusion 50 questions practice/18_MinimumOfArrayUsingRecrusion.cpp
+++ Problem_Solving_CPP/Learning/AS/04_Recursion/recrusion 50 questions practice/18_MinimumOfArrayUsingRecrusion.cpp
@@ -4,22 +4,33 @@
 #include<iostream>
 using namespace std;
 
-int min (vector<int>arr){
-    static int start = 0;
-    static int end = arr.size()-1;
-    static int minNum = INT_MAX;
+// int min (vector<int>arr){
+//     static int start = 0;
+//     static int end = arr.size()-1;
+//     static int minNum = INT_MAX;
 
-    if (start>end){
-        return minNum;
+//     if (start>end){
+//         return minNum;
+//     }
+//     if (arr[start]<minNum){
+//         minNum = arr[start];
+//     }
+//     start++;
+//     return min(arr);
+// }
+
+
+// method 2 - short and good method - without static int 
+
+int mini (vector<int>arr, int index){
+    if (index > arr.size()){
+        return arr[index];
     }
-    if (arr[start]<minNum){
-        minNum = arr[start];
-    }
-    start++;
-    return min(arr);
+
+    return min (arr[index], mini(arr , index+1));
 }
 
 int main (){
-    vector<int>arr = {1,0,2,4,9,-1};
-    cout<<min(arr);
+    vector<int>arr = {1,0,2,4,9,-1,10};
+    cout<<mini(arr, 0);
 }
