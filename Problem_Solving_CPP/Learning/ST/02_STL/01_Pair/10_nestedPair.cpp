ec6cc60d810b1d94c444d3ce1093b00ff2804da0
LateBloomer34 <himnariyal30@gmail.com>
2025-12-23 00:00:35 +0530

    nested pair

--- Problem_Solving_CPP/Learning/ST/02_STL/01_Pair/10_nestedPair.cpp
+++ Problem_Solving_CPP/Learning/ST/02_STL/01_Pair/10_nestedPair.cpp
@@ -8,4 +8,13 @@ using namespace std;
 int main (){
     pair<int, pair<int, int>>p = {1,{1,2}};
     cout<<p.first<<" "<<p.second.first<<" "<<p.second.second;
+    
+    // other method
+
+    // auto [a, inner] = p;
+    // auto [b, c] = inner;
+
+    // cout << a << " " << b << " " << c << endl;
 }
+
+
