515027d80f9517f177a277658d9c2449726882f3
LateBloomer34 <himnariyal30@gmail.com>
2025-12-22 23:54:07 +0530

    sum and pair store in a pair

--- Problem_Solving_CPP/Learning/ST/02_STL/01_Pair/09_SumAndDiffOfValue.cpp
+++ Problem_Solving_CPP/Learning/ST/02_STL/01_Pair/09_SumAndDiffOfValue.cpp
@@ -3,6 +3,20 @@
 #include<iostream>
 using namespace std;
 
+pair<int , int> calc(int n1, int n2){
+    int diff = n1-n2;
+    int sum = n1 + n2;
+
+    return {sum , diff};
+
+}
+
 int main (){
-        
+        int n1 = 6; 
+        int n2 = 3;
+
+        pair<int , int>result = calc(n1, n2);
+
+        cout<<"sum is " <<result.first<<endl;
+        cout<<"diff is "<< result.second;
 }
