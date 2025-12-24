110a01caef3b8250e15303abda635a98842bc4e0
LateBloomer34 <himnariyal30@gmail.com>
2025-12-24 15:42:36 +0530

    list

---
+++ Problem_Solving_CPP/Learning/ST/02_STL/03_list/01_listProb.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+
+int main (){
+    list<int>li = {1,12,3,4};
+    int n = li.size();
+   
+    // print the list
+    // for (auto i = li.begin(); i <= li.end(); i++){
+    //     cout<<*i<<" ";
+    // }
+    cout<<*li.begin()+1;
+    // for (auto i :li){
+    //     cout<<i<<" ";
+    // }
+    cout<<endl;
+}
