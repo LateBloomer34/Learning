01c42ec08c431f0bc920a97884b88f505a6723e9
LateBloomer34 <himnariyal30@gmail.com>
2026-01-01 10:27:07 +0530

    find element in sorted array using binary search

---
+++ Problem_Solving_CPP/Learning/aditya verma/Binary Search/01_BinarySearch.cpp
@@ -0,0 +1,33 @@
+// find element in a sorted array
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int search(vector<int> &arr ,int target){
+    int n = arr.size();
+
+    int start = 0 ; 
+    int end  = n-1;
+
+    while(start<end){
+        int mid = start+(end-start)/2;
+
+        if (arr[mid] == target){
+            return mid;
+        }
+        else if (target<arr[mid]){
+            end = mid-1;
+        }
+        else{
+            start = mid+1;
+        }
+    }
+    return -1;
+}
+
+int main (){
+    vector<int>arr = {1,2,3,4,5,6,7,8};
+    int target = 6;
+    cout<<search(arr, target);
+}
